6287d72fb20bcdc8b40c04cdd941c368d9f8b305
Hardynamakhwa <157681178+Hardynamakhwa@users.noreply.github.com>
2024-02-27 12:04:22 +0000

    update index screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,25 +1,31 @@
 import { Link, Stack, router } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { Text, View, Pressable } from "react-native";
-
+import Combobox from "../components/Combobox";
+import { useState } from "react";
 
 export default function Page() {
-    const handleImport = () => {
-        router.replace('/import');
-    }
-    const handleSetup = () => {
-        router.replace('/setup');
-    }
+  const [mnemonic, _mnemonic] = useState<string>("");
 
-    return (
-        <View className="flex-1 p-4 items-center justify-end dark:bg-black">
-            <Pressable onPress={handleSetup} className='w-full items-center justify-center p-3 rounded-xl bg-gray-800 dark:bg-white'>
-                <Text className='text-white font-medium dark:text-gray-800 capitalize'>Create account</Text>
-            </Pressable>
-            <Pressable onPress={handleImport} className='w-full items-center justify-center p-3 rounded-xl mt-2 bg-gray-800 dark:bg-white'>
-                <Text className='text-white font-medium dark:text-gray-800 capitalize'>import</Text>
-            </Pressable>
-            <StatusBar style="auto" />
-        </View>
-    );
+  const handleMnemonicChange = (value: string) => {
+    _mnemonic(value);
+  };
+
+  return (
+    <View className="flex-1 flex p-4 items-center justify-end dark:bg-black">
+      <View className="flex-1 w-full justify-center">
+        <Text className="font-semibold mb-2 dark:text-white">Seed phrase</Text>
+        <Combobox onChangeText={handleMnemonicChange} />
+        <Pressable className="w-full items-center justify-center mt-3 p-3 rounded-full bg-gray-800 dark:bg-white">
+          <Text className="text-white font-medium dark:text-gray-800">
+            Import address
+          </Text>
+        </Pressable>
+      </View>
+      <Pressable className="w-full items-center justify-center p-3 rounded-full mt-auto bg-gray-800 dark:bg-gary-800 border dark:border-gray-700">
+        <Text className="text-gray-50 font-medium">Create new address</Text>
+      </Pressable>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
---
+++ components/Combobox.tsx
@@ -0,0 +1,64 @@
+import { Feather } from "@expo/vector-icons";
+import _ from "lodash";
+import { createRef, useState } from "react";
+import { View, Text, TextInput } from "react-native";
+
+interface ComboboxProps {
+  onChangeText: (value: string) => void;
+}
+
+export default function Combobox({ onChangeText }: ComboboxProps) {
+  const inputRef = createRef<TextInput>();
+  const [input, _input] = useState("");
+  const [chips, _chips] = useState<string[]>([]);
+
+  const handleInput = (value: string) => {
+    const newChips = value.split(" ");
+    const lastIndex = _.size(newChips) - 1;
+
+    _chips(
+      _.chain(chips)
+        .union(_.chain(newChips).splice(0, lastIndex).value())
+        .value()
+    );
+    _input(newChips[lastIndex]);
+    onChangeText(_.chain(chips).concat([input]).join(" ").value());
+  };
+
+  return (
+    <View className="flex flex-row flex-wrap w-full gap-1 p-2 rounded-xl shadow-lg border border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-900">
+      {_.map(chips, (chip, index) => (
+        <View
+          key={index}
+          className="inline-flex items-center flex-row gap-x-2 p-2 rounded-xl border border-gray-300 dark:border-gray-700 dark:bg-gray-800"
+        >
+          <Text className="text-sm text-gray-300 dark:text-gray-500">
+            {index + 1}
+          </Text>
+          <Text className="font-semibold text-gray-900 dark:text-gray-100">
+            {chip}
+          </Text>
+          <Text className="text-gray-900 dark:text-gray-300">
+            <Feather
+              name="x-circle"
+              size={16}
+              onPress={() => _chips(_.difference(chips, [chip]))}
+            />
+          </Text>
+        </View>
+      ))}
+      {_.size(chips) + 1 <= 12 && (
+        <TextInput
+          placeholder={"word " + (_.size(chips) + 1)}
+          placeholderTextColor="#6B7280"
+          className="mix-w-[80px] flex-1 text-gray-900 dark:text-gray-100"
+          ref={inputRef}
+          value={input}
+          autoCorrect={false}
+          autoCapitalize="none"
+          onChangeText={handleInput}
+        />
+      )}
+    </View>
+  );
+}
