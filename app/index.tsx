cce5a7638be867a287fd24defaa1211c190e7a1e
Hardy Namakhwa <66388667+Hard265@users.noreply.github.com>
2024-03-06 07:06:02 +0200

    Update index.tsx
    clean the screen

--- app/index.tsx
+++ app/index.tsx
@@ -2,30 +2,15 @@ import { router } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
 import { Pressable, TextStyle, View } from "react-native";
-import Combobox from "../components/Combobox";
 import { TextBlack, TextMedium, TextSemiBold } from "../components/Text";
 import { Button, Chip, TextInput, useTheme } from "react-native-paper";
 import { TextButtonStyle } from "../misc/styles";
 import _ from "lodash";
 
 export default function Page() {
-  const [mnemonic, _mnemonic] = useState<string>("");
-  const [chips, _chips] = useState<string[]>([]);
-
+  const [mnemonic, _mnemonic] = useState("");
+  
   const theme = useTheme();
-
-  const handleMnemonicChange = (value: string) => {
-    const newChips = value.split(" ");
-    const lastIndex = _.size(newChips) - 1;
-
-    _chips(
-      _.chain(chips)
-        .union(_.chain(newChips).splice(0, lastIndex).value())
-        .value()
-    );
-    _mnemonic(newChips[lastIndex]);
-    // onChangeText(_.chain(chips).concat([input]).join(" ").value());
-  };
 
   return (
     <View className="flex-1 flex p-4 items-center justify-end dark:bg-black">
@@ -38,33 +23,27 @@ export default function Page() {
           address
         </TextMedium>
       </View>
-      <View className="w-full justify-center">
-        {/* <Combobox onChangeText={handleMnemonicChange} /> */}
+      <View
+        className="w-full justify-center"
+      >
         <TextInput
           label="Seed phrase"
           value={mnemonic}
           mode="outlined"
-          // numberOfLines={3}
-          // multiline
-          // render={(props) => {
-          //   return (
-          //     <View className="flex flex-row flex-wrap">
-          //       {_.map(chips, (chip, index) => (
-          //         <Chip  key={index}>{props.value}</Chip>
-          //       ))}
-          //       <TextInput
-          //         value={props.value}
-          //         onChangeText={handleMnemonicChange}
-          //       />
-          //     </View>
-          //   );
-          // }}
-          onChangeText={handleMnemonicChange}
+          multiline
+          numberOfLines={3}
+          contentStyle={{
+            paddingTop: 14,
+            paddingBottom: 14
+          }}
+          onChangeText={
+            handleMnemonicChange
+          }
         />
 
         <Button
           disabled={_.isEmpty(mnemonic)}
-          className="rounded-lg mt-2 p-1"
+          className="rounded-lg mt-2 p-0.5"
           labelStyle={[TextButtonStyle]}
           mode="contained"
           onPress={() => router.push("/chat")}
@@ -73,7 +52,7 @@ export default function Page() {
         </Button>
 
         <Button
-          className="rounded-lg mt-4 p-1"
+          className="rounded-lg mt-4 p-0.5"
           labelStyle={[TextButtonStyle]}
           mode="contained-tonal"
         >
